251c8876474a47be1bacad59a41e3544dbda1efe
Cuong Nguyen <cuong.shodan@gmail.com>
2024-11-04 21:42:54 +0100

    prepare battlefield

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from "./pages/Home/Home"
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import SignIn from './pages/Signin/SignIn'
 
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <body>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin/" element={<SignIn />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </body>
+
   )
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,23 +1,8 @@
+import { Link } from "react-router-dom"
 
 const Home = () => {
     return (
-        <body>
-            <nav className="main-nav">
-                <a className="main-nav-logo" href="./index.html">
-                    <img
-                        className="main-nav-logo-image"
-                        src="/img/argentBankLogo.png"
-                        alt="Argent Bank Logo"
-                    />
-                    <h1 className="sr-only">Argent Bank</h1>
-                </a>
-                <div>
-                    <a className="main-nav-item" href="./sign-in.html">
-                        <i className="fa fa-user-circle"></i>
-                        Sign In
-                    </a>
-                </div>
-            </nav>
+        <>
             <main>
                 <div className="hero">
                     <section className="hero-content">
@@ -63,10 +48,7 @@ const Home = () => {
                     </div>
                 </section>
             </main>
-            <footer className="footer">
-                <p className="footer-text">Copyright 2020 Argent Bank</p>
-            </footer>
-        </body>
+        </>
     )
 }
 
